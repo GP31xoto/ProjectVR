e45e71bd71ee45907f1ed4a8e3bec799a2d623d3
GP31xoto <75365892+GP31xoto@users.noreply.github.com>
2021-08-07 21:01:25 +0100

    House script, materials & prefabs edits

--- TestProject1/Assets/Scripts/House.cs
+++ TestProject1/Assets/Scripts/House.cs
@@ -4,18 +4,44 @@ using UnityEngine;
 
 public class House : MonoBehaviour
 {
-    private ConstructionCost cost;
-    public int type;
+    protected ConstructionCost cost;
+    [SerializeField] private int type;
+    public float effect;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        switch (type)
+        {
+            case 1:
+                cost = new ConstructionCost(5, true);
+                effect = 0.05f;
+                break;
+            case 2:
+                cost = new ConstructionCost(3, 4);
+                effect = 0.07f;
+                break;
+            case 3:
+                cost = new ConstructionCost(4, 2);
+                effect = 0.1f;
+                break;
+            case 4:
+                cost = new ConstructionCost(5, 5);
+                effect = 0.14f;
+                break;
+            case 5:
+                cost = new ConstructionCost(7, 3);
+                effect = 0.16f;
+                break;
+            case 6:
+                cost = new ConstructionCost(6, 8);
+                effect = 0.2f;
+                break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public float GiveEffect()
     {
-        
+        return effect;
     }
 }
