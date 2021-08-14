db163a609d2ccf63f37025258c46c0c3ff857921
dj120599 <60100701+dj120599@users.noreply.github.com>
2021-08-14 18:37:42 +0100

    SoundMenu

--- TestProject1/Assets/Scripts/Pause_Menu/Sound_Script.cs
+++ TestProject1/Assets/Scripts/Pause_Menu/Sound_Script.cs
@@ -2,21 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class Sound_Script : MonoBehaviour
 {
-public GameObject pausemenu;
-public GameObject soundmenu;
+ public GameObject pausemenu;
+ public GameObject soundmenu;
+
+ public Slider mySliderGen;
+ public Slider mySliderMusic;
+ public Slider mySliderSound;
+
+
 
  public void PauseMenu()
-{
-pausemenu.SetActive(true);
-soundmenu.SetActive(false);
-}
+ {
+ pausemenu.SetActive(true);
+ soundmenu.SetActive(false);
+ }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
+  public void OnGenValueChange()
+  {
+
+   if(mySliderGen.value < mySliderMusic.value)
+   {
+   mySliderMusic.value = mySliderGen.value;         
+   }
+   else if(mySliderGen.value < mySliderSound.value)
+   {
+   mySliderMusic.value = mySliderGen.value;
+   }
+
+  }
+
+  public void OnMusicValueChange()
+  {
+
+   if(mySliderGen.value < mySliderMusic.value)
+   {
+   mySliderMusic.value = mySliderGen.value;         
+   }
+ 
+  }
+
+  public void OnSoundValueChange()
+  {
+
+   if(mySliderGen.value < mySliderSound.value)
+   {
+   mySliderMusic.value = mySliderGen.value;
+   }
+
+  }
 }
