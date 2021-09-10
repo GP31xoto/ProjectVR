53ea62cc39e49bdf7cdc700035d6da828df8a686
GP31xoto <75365892+GP31xoto@users.noreply.github.com>
2021-09-10 18:37:48 +0100

    counters

--- TestProject1/Assets/Scripts/ResourceHUD.cs
+++ TestProject1/Assets/Scripts/ResourceHUD.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ResourceHUD : MonoBehaviour
 {
@@ -8,14 +9,19 @@ public class ResourceHUD : MonoBehaviour
     [SerializeField] private GameObject GameFlow;
     [SerializeField] private GameObject foodCounter;
     [SerializeField] private Material foodCounterMaterial;
+    [SerializeField] private Text foodCounterNumber;
     [SerializeField] private GameObject woodCounter;
     [SerializeField] private Material woodCounterMaterial;
+    [SerializeField] private Text woodCounterNumber;
     [SerializeField] private GameObject ironCounter;
     [SerializeField] private Material ironCounterMaterial;
+    [SerializeField] private Text ironCounterNumber;
     [SerializeField] private GameObject waterCounter;
     [SerializeField] private Material waterCounterMaterial;
+    [SerializeField] private Text waterCounterNumber;
     [SerializeField] private GameObject populationCounter;
     [SerializeField] private Material populationCounterMaterial;
+    [SerializeField] private Text populationCounterNumber;
 
     // Start is called before the first frame update
     void Start()
@@ -35,6 +41,7 @@ public class ResourceHUD : MonoBehaviour
             foodCounterMaterial.color = Color.red;
         }
         foodCounter.transform.localScale = new Vector3((float)GameFlow.GetComponent<GameFlow>().food, 30, 1);
+        foodCounterNumber.text = GameFlow.GetComponent<GameFlow>().food.ToString();
         if (GameFlow.GetComponent<GameFlow>().wood >= 25f)
         {
             woodCounterMaterial.color = Color.green;
@@ -44,6 +51,7 @@ public class ResourceHUD : MonoBehaviour
             woodCounterMaterial.color = Color.red;
         }
         woodCounter.transform.localScale = new Vector3((float)GameFlow.GetComponent<GameFlow>().wood, 30, 1);
+        woodCounterNumber.text = GameFlow.GetComponent<GameFlow>().wood.ToString();
         if (GameFlow.GetComponent<GameFlow>().iron >= 25f)
         {
             ironCounterMaterial.color = Color.green;
@@ -53,6 +61,7 @@ public class ResourceHUD : MonoBehaviour
             ironCounterMaterial.color = Color.red;
         }
         ironCounter.transform.localScale = new Vector3((float)GameFlow.GetComponent<GameFlow>().iron, 30, 1);
+        ironCounterNumber.text = GameFlow.GetComponent<GameFlow>().iron.ToString();
         if (GameFlow.GetComponent<GameFlow>().water >= 25f)
         {
             waterCounterMaterial.color = Color.green;
@@ -62,6 +71,7 @@ public class ResourceHUD : MonoBehaviour
             waterCounterMaterial.color = Color.red;
         }
         waterCounter.transform.localScale = new Vector3((float)GameFlow.GetComponent<GameFlow>().water, 30, 1);
+        waterCounterNumber.text = GameFlow.GetComponent<GameFlow>().food.ToString();
         if (GameFlow.GetComponent<GameFlow>().population >= 25)
         {
             populationCounterMaterial.color = Color.green;
@@ -71,5 +81,6 @@ public class ResourceHUD : MonoBehaviour
             populationCounterMaterial.color = Color.red;
         }
         populationCounter.transform.localScale = new Vector3((float)GameFlow.GetComponent<GameFlow>().population, 30, 1);
+        populationCounterNumber.text = GameFlow.GetComponent<GameFlow>().population.ToString();
     }
 }
